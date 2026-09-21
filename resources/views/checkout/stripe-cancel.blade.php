527a5eea3977ace51546dacc0626c7738acbbffc
Agostinho Borges <agostinho-borges24>
2026-09-21 13:42:30 +0100

    feat: add Stripe cancellation view to inform users of payment status

---
+++ resources/views/checkout/stripe-cancel.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="checkout-result">
+        <h1>Pagamento cancelado</h1>
+        <p>O pagamento do pedido <strong>{{ $order->order_number }}</strong> foi cancelado. Nada foi cobrado.</p>
+        <a href="{{ route('checkout.stripe.start', $order) }}" class="btn btn--primary">Tentar novamente</a>
+    </div>
+</x-app-layout>
